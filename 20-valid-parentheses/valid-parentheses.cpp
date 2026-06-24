bd6954740d044451636c5412248ec972b4487bee
TEJAS_AGRAWAL <tamusic2147k@gmail.com>
2026-06-24 20:00:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (88.97%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       if(s.size()==1)return false;
+       stack<char>st;
+       for(int i =0;i<s.size();i++){
+        if(st.empty()){
+            st.push(s[i]);
+            continue;
+        }
+        if(st.top()=='(' && s[i]==')')st.pop();
+        else if(st.top()=='[' && s[i]==']')st.pop();
+        else if(st.top()=='{' && s[i]=='}')st.pop();
+        else st.push(s[i]);
+       }
+       if(st.size())return false;
+        return true;
+    }
+};
