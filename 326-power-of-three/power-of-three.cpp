68c46cbb4a98533c5fd100c0ae4b5757e5342e26
TEJAS_AGRAWAL <tamusic2147k@gmail.com>
2026-06-28 19:15:22 +0530

    Time: 2 ms (64.45%) | Memory: 8.9 MB (42.29%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-
-    bool three(int n ){
-        if(n==0)return 0;
-        if(n==1)return 1;
-        if(n%3!=0)return 0;
-        n/=3;
-       return three(n);
-    }
-
     bool isPowerOfThree(int n) {
-        if(n<=0)return 0;
-        return three(n);
+        return (n>0 && 1162261467%n==0);
     }
 };
